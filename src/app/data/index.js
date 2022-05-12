9a8e46ee35d1bc3f45add69f33b09e2a708ff655
Maliheh Ghasemian <malih.ghasemian@gmail.com>
2022-05-12 14:48:27 +0200

    feat : Move data to a new file.

---
+++ src/app/data/index.js
@@ -0,0 +1,29 @@
+export const data = [
+  {
+    id: 1,
+    from: 0,
+    to: 1.23,
+    color: "rgb(225, 157, 157)",
+    pattern: "/assets/15104018.svg",
+    title: "Jura des Juragebirges",
+    subtitle: "Dazit",
+  },
+  {
+    id: 2,
+    from: 1.23,
+    to: 1.77,
+    color: "#789123",
+    pattern: "/assets/15102042.svg",
+    title: "Quartar, undifferenziert",
+    subtitle: "Phonolith",
+  },
+  {
+    id: 3,
+    from: 1.77,
+    to: 2.34,
+    color: "#456789",
+    pattern: "/assets/15102034.svg",
+    title: "Altere Ablagerungen, undifferenziert",
+    subtitle: "Magmatit",
+  },
+];
