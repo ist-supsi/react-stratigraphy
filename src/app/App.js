5246b541b7232797a9e67936d709d3e0fdfb9e42
Maliheh Ghasemian <malih.ghasemian@gmail.com>
2022-05-18 09:50:52 +0200

    feat : Using FirstModel in App.

--- src/app/App.js
+++ src/app/App.js
@@ -1,25 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import * as Styled from "./styles";
-import Stratigraphy from "../features/stratigraphy";
-import { data } from "./data";
+import FirstModel from "../features/multipleStratigraphy/firstModel";
 
 const App = () => {
-  const [selected, setSelected] = useState(null);
-
   return (
     <Styled.Container>
-      <Stratigraphy
-        data={data}
-        onSelected={(layer) => {
-          setSelected(layer);
-        }}
-        unit="m"
-      />
-      {/* half left side of the page that shows the title */}
-      <Styled.Description selected={selected !== null}>
-        <Styled.Title>{selected?.title}</Styled.Title>
-        <Styled.Subtitle>{selected?.subtitle}</Styled.Subtitle>
-      </Styled.Description>
+      <FirstModel />
     </Styled.Container>
   );
 };
--- src/app/styles.js
+++ src/app/styles.js
@@ -10,25 +10,3 @@ export const Container = styled.div`
   border: thin solid #d0d0d0;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.4);
 `;
-
-export const Description = styled.div`
-  flex: 1 1 100%;
-  padding: 1rem;
-  border: ${(props) =>
-    props.selected === true ? "2px solid #f00" : "transparent"};
-  background-color: ${(props) =>
-    props.selected === true ? "rgb(233, 233, 233)" : "transparent"};
-  border-left: none;
-  transition: all 0.25s cubic-bezier(0.25, 0.8, 0.25, 1);
-`;
-
-export const Subtitle = styled.div`
-  font-size: 1.2rem;
-  font-weight: bold;
-  color: #787878;
-`;
-
-export const Title = styled.div`
-  font-size: 2rem;
-  font-weight: bold;
-`;
