9a20725ce3c6067216767d1d1d98c983e7608919
Maliheh Ghasemian <malih.ghasemian@gmail.com>
2022-05-18 12:44:05 +0200

    feat : Change data for considering casing and filling layer.

--- src/features/multipleStratigraphy/data/index.js
+++ src/features/multipleStratigraphy/data/index.js
@@ -20,10 +20,69 @@ export const data = [
   {
     id: 3,
     from: 1.77,
-    to: 2.34,
+    to: 5.34,
     color: "#456789",
     pattern: "/assets/15102034.svg",
     title: "Altere Ablagerungen, undifferenziert",
     subtitle: "Magmatit",
+  },
+];
+
+export const dataCasing = [
+  {
+    id: 1,
+    from: 0,
+    to: 1,
+    color: "#ff5733",
+    pattern: "/assets/15102042.svg",
+    title: "Casing 1",
+    subtitle: "Dazit",
+  },
+  {
+    id: 2,
+    from: 1,
+    to: 1.77,
+    color: "#3377ff",
+    pattern: "/assets/15104018.svg",
+    title: "Casing 2",
+    subtitle: "Phonolith",
+  },
+  {
+    id: 3,
+    from: 1.77,
+    to: 2,
+    color: "#d733ff",
+    pattern: "/assets/15102034.svg",
+    title: "Altere + Casing 3",
+    subtitle: "Magmatit",
+  },
+];
+export const dataFilling = [
+  {
+    id: 0,
+    from: 0,
+    to: 1.23,
+    color: "#33ffff",
+    pattern: "/assets/15102034.svg",
+    title: "Filling 1",
+    subtitle: "Dazit",
+  },
+  {
+    id: 1,
+    from: 1.77,
+    to: 2.34,
+    color: "#456789",
+    pattern: "/assets/15104018.svg",
+    title: "Filling 2",
+    subtitle: "Magmatit",
+  },
+  {
+    id: 2,
+    from: 1.23,
+    to: 1.77,
+    color: "#789123",
+    pattern: "/assets/15102042.svg",
+    title: "Quartar, undifferenziert",
+    subtitle: "Phonolith",
   },
 ];
